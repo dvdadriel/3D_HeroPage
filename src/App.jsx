fff683d641cf219b56f85f17234b86bc345f31b8
“David” <davidadrielalvyn@gmail.com>
2025-10-20 17:20:27 +0700

    commit project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Aos from "aos"
+import "aos/dist/aos.css"
+import { use, useEffect } from "react"
+
+export default function App() {
+  useEffect(() => {
+    Aos.init({ duration: 1500, once: true })
+  }, [])
+  return (
+    <main>
+      {/* Gradient Image */}
+      <img src="/gradient.png" alt="Gradient-img" className="absolute top-0 right-0 opacity-60 -z-10" />
+
+      {/* Blur Effect */}
+      <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+
+      <Header />
+      <Hero />
+    </main>
+  )
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,47 @@
+import 'boxicons/css/boxicons.min.css';
+import Spline from '@splinetool/react-spline';
+
+const Hero = () => {
+  return (
+    <main className="flex lg:mt-2 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
+        <div data-aos="fade-right" data-aos-effect="300" data-aos-easing="ease-in-sine" className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
+            {/* Tag box */}
+            <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
+                <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
+                    <i className='bx bx-diamond'></i>
+                    INTRODUCING
+                </div>
+            </div>
+
+            {/* Main Heading */}
+            <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider my-8'>
+                EMAIL FOR
+                <br />
+                DEVELOPERS
+            </h1>
+
+            {/* Description */}
+            <p className='text-base sm:text-lg tracking-wider text-gray-400 max-w-[25rem] lg:max-w-[30rem]'>
+                The best way to reach humans instead of spam folders, deliver transactional and marketing emails at scale
+            </p>
+
+            {/* Button */}
+            <div className='flex gap-4 mt-12'>
+                <a href="#" className='border border-[#2a2a2a] py-2 sm:py-3 px-4 sm:px-5 rounded-full sm:text-lg text-sm font-semibold transition-all duration-300 hover:bg[#1a1a1a]'>
+                    Documentation
+                    <i className='bx bx-link-external'></i>
+                </a>
+                <a href="#" className='border border-[#2a2a2a] py-2 sm:py-3 px-8 sm:px-10 rounded-full sm:text-lg text-sm font-semibold transition-all duration-300 hover:bg[#1a1a1a] bg-gray-300 text-black hover:text-white'>
+                    Get Started
+                    <i className='bx bx-link-external'></i>
+                </a>
+            </div>
+        </div>
+
+        {/* 3D Robot */}
+        <Spline data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0" data-aos-duration="3000" className="absolute lg:top-0 top-[-20%] bottom-0 lg:left-[25%] sm:left-[2%] h-full" scene="https://prod.spline.design/OL0m7OWpEZwOkuJv/scene.splinecode" />
+    </main>
+  )
+}
+
+export default Hero
